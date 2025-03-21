e148b2db19847eba12d032051a35d44b2c07b95c
Pedro Raimundo <raimundo.pedro2004@gmail.com>
2025-03-21 10:05:57 +0000

    antes do vieew

--- Empresa/Controllers/ContatosEmpresaController.cs
+++ Empresa/Controllers/ContatosEmpresaController.cs
@@ -31,5 +31,33 @@ namespace Empresa.Controllers
             var contatos = await _AppDbcontext.Contatos.ToListAsync();
             return Ok(contatos);
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var contato = await _AppDbcontext.Contatos.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(contato);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var contato = await _AppDbcontext.Contatos.FirstOrDefaultAsync(x => x.Id == id);
+
+            if (contato == null) return NotFound();
+
+            _AppDbcontext.Contatos.Remove(contato);
+            await _AppDbcontext.SaveChangesAsync();
+            return Ok(contato);
+
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(ContatosEmpresa contato)
+        {
+            _AppDbcontext.Contatos.Update(contato);
+            await _AppDbcontext.SaveChangesAsync();
+            return Ok(contato);
+        }
     }
 }
